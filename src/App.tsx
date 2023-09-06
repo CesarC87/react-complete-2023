d4fb0452d4d54f8da4f34d9e66a320f17918fd9c
Cesar <ccarabetta@cdt.com.ar>
2023-09-05 22:13:24 -0300

    Pull de files en 81

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 // import { useState } from 'react'
 import './App.css'
 import Expenses from './components/Expenses/Expenses'
+import ExpensesFilter from './components/ExpensesFilter/ExpensesFilter'
+import NewExpense from './components/NewExpense/NewExpense'
 
 function App() {
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <>
+      <ExpensesFilter/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses/>
     </>
   )
